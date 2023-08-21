9baf557310c5961ff16ca66eee9bccf4360602e9
RubyShanks <sanketjaipuriar@gmail.com>
2023-08-21 23:05:57 +0530

    aritho.py: Renamed some variables for readability

--- aritho.py
+++ aritho.py
@@ -4,48 +4,64 @@ import time
 import datetime
 
 session_start = datetime.datetime.now()
-nums = range(int(1e4))
-score = [0, 0]
-totresptime = 0
+print('Choose difficulty:')
+print('1: Vanilla')
+print('2: Amateur 1')
+print('3: Amateur 2')
+print('4: Challenger')
+print('5: Expert')
+print('6: Pro')
+print('7: World Class')
+print('8: World Champion')
+print('9: Insane')
+print('10: Legendary')
+diff = int(input())
+num_limit = 10 ** diff
+score = 0
 resptimes = []
-try:
+total_attempts = 0
+os.system('cls')
+try: # If an error occurs program should show results before quitting. 
     while True:
-        x = random.choice(nums)
-        y = random.choice(nums)
+        num1 = random.choice(range(num_limit))
+        num2 = random.choice(range(num_limit))
         print('Enter -1 to quit')
-        print(x, '+', y, '= ', end='')
-        before = datetime.datetime.now()
-        ans = int(input())
-        after = datetime.datetime.now()
-        interval = after - before
-        totresptime += interval.total_seconds()
-        resptimes.append(interval.total_seconds())
-        if ans == -1:
+        print(num1, '+', num2, '= ', end='')
+        before_response = datetime.datetime.now()
+        answer = int(input())
+        after_response = datetime.datetime.now()
+        response_time = after_response - before_response
+        if answer == -1:
             break
-        if ans == (x + y):
+        resptimes.append(response_time.total_seconds())
+        if answer == (num1 + num2):
             print ('Correct!')
-            score[0] += 1
+            score += 1
         else:
             print('Wrong!')
-        print('Response Time:', round(interval.total_seconds(), 2), end='')
+        print('Response Time:', round(response_time.total_seconds(), 2), end='')
         print('s')
-        score[1] += 1
+        total_attempts += 1
         time.sleep(1.5)
         os.system('cls')
 finally:
-    resptimes.sort()
-    print('Total                : ', score[1])
+    totresptime = 0
+    for resptime in resptimes:
+        totresptime += resptime
+    resptimes.sort() # Sorted to find median of data
+    print('Total                : ', total_attempts)
     try:
-        print('Accuracy             :', round(score[0] * 100 / score[1], 1), end='')
+        print('Accuracy             :', round(score * 100 / total_attempts, 1), end='')
         print('%')
         print('Median response time :', round(resptimes[int(len(resptimes) / 2)], 1), end='')
         print('s')
-        print('Average response time:', round(totresptime / score[1], 1), end='')
+        print('Average response time:', round(totresptime / total_attempts, 1), end='')
         print('s')
     finally:
+        print('Terminating Program.')
         print('Thanks for Playing!')
         session_end = datetime.datetime.now()
-        interval = session_end - session_start
-        print('Session Time:', int(interval.total_seconds() / 60), 'minutes')
+        session_time = session_end - session_start
+        print('Session Time:', int(session_time.total_seconds() / 60), 'minutes')
         time.sleep(3)
         

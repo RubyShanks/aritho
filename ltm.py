1cfb9c4b7621814b5de97e08c2f1a941bb956f27
RubyShanks <sanketjaipuriar@gmail.com>
2023-09-01 17:28:08 +0530

    ltm.py|implemented average level|increased starting level

--- ltm.py
+++ ltm.py
@@ -7,12 +7,14 @@ import time
 
 os.system('cls')
 
-total_digits = 6
-time_limit = 5
+initial_level = 10
+total_digits = initial_level
+time_limit = 7
 extra_time = 0.5
 attempts = 0
 score = 0
-best_level = 6
+best_level = initial_level
+sum_levels = 0
 try:
     while (True):
         os.system('cls')
@@ -31,6 +33,7 @@ try:
         for num, ans in zip(nums, answer):
             if (num != int(ans) or len(nums) != len(answer)):
                 print('Wrong Answer!')
+                sum_levels += total_digits - 1
                 total_digits -= 1
                 time_limit -= extra_time
                 print('Correct Answer:', end=' ')
@@ -54,6 +57,8 @@ finally:
     print('Score:', score)
     print('Max level reached:', best_level)
     try: 
+        if attempts != score:
+            print('Average level    :', round(sum_levels / (attempts - score), 2))
         print('Accuracy:', round((score/attempts) * 100, 2))
     finally:
         print('Terminating Program')
